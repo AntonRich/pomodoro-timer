c5f524273184a9bcc67fdd063fbd5c67070db94a
Vince Campanale <vcampanale0294@gmail.com>
2016-12-04 23:42:51 -0500

    fully functional, but not DRY - code smell issues. missing a few edge cases

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
 var startPauseText = document.getElementById("startPauseText");
 var countdown = document.getElementById("countdown");
 var title = document.getElementById("title");
-var sessionLength = document.getElementById("");
+var sessionLength = document.getElementById("sessionLength");
+var breakLength = document.getElementById("breakLength");
 
-var timerType; //possible values = work, break (default is work)
+var timerType; //possible values = session, break (default is session)
 var id;
+var initialSessionTime;
+var initialBreakTime;
 
-//initial display value of 25 minutes, type = work
+
+//initial display value of 25 minutes, type = session
 window.onload = function(){
   countdown.textContent = "00:03";
+  sessionLength.textContent = "25";
+  initialSessionTime = countdown.textContent;
+
+  breakLength.textContent = "10";
+  initialBreakTime = "10:00";
+
   startPauseText.innerHTML = "Start"
-  timerType = "work";
+  timerType = "session";
   loadType();
 };
 
 function loadType(){
   var main = document.getElementById("main");
-  if(timerType === "work"){
-    main.className = "work-mode";
-    title.textContent = "Work";
+  if(timerType === "session"){
+    main.className = "session-mode";
+    title.textContent = "Session";
   }
   if(timerType === "break"){
     main.className += " break-mode";
@@ -28,23 +38,24 @@ function loadType(){
 
 function startTimer(duration) {
   startPauseText.innerHTML = "Pause";
-  var minutes, seconds;
-  minutes = Math.floor(duration / 60);
-  seconds = duration % 60;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
-  countdown.innerHTML = minutes + ":" + seconds;
+  countdown.innerHTML = convertToString(duration);
+  // var minutes, seconds;
+  // minutes = Math.floor(duration / 60);
+  // seconds = duration % 60;
+  // minutes = minutes < 10 ? "0" + minutes : minutes;
+  // seconds = seconds < 10 ? "0" + seconds : seconds; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
+  // countdown.innerHTML = minutes + ":" + seconds;
   duration = duration - 1;
   id = setTimeout(function () {
       startTimer(duration);
   }, 1000);
 
-  if(timerType === "work" && (countdown.textContent === "00:00")){
+  if(timerType === "session" && (countdown.textContent === "00:00")){
     setTimeout(function(){
       pauseTimer();
       timerType = "break";
       loadType();
-      countdown.textContent = "10:00";
+      countdown.textContent = initialBreakTime;
       var breaktime = convertToTime(countdown.textContent);
       startTimer(breaktime);
     }, 1000);
@@ -59,8 +70,12 @@ function pauseTimer() {
 
 function resetTimer(){
   pauseTimer();
-  countdown.textContent = "00:10";
-  timerType = "work";
+  if(timerType === "session"){
+    countdown.textContent = initialSessionTime;
+  }
+  if(timerType === "break"){
+    countdown.textContent = initialBreakTime;
+  }
 }
 
 //multiple return statements less than ideal, but unsure how to handle this otherwise
@@ -84,8 +99,75 @@ startPauseButton.addEventListener("click", function(){
 var resetButton = document.getElementById("reset");
 resetButton.addEventListener("click", function(){
   resetTimer();
+  timerType = "session";
+  loadType();
 });
 
+//Add time, subtract time functions
+var sessionPlus = document.getElementById("sessionPlus");
+sessionPlus.addEventListener("click", function(){
+  resetTimer();
+  var currentValue = convertToTime(initialSessionTime);
+  currentValue += 60;
+  initialSessionTime = convertToString(currentValue);
+  countdown.innerHTML = initialSessionTime;
+  if(initialSessionTime.charAt(0) !== "0"){
+    sessionLength.textContent = initialSessionTime.substring(0,2);
+  } else {
+    sessionLength.textContent = initialSessionTime.substring(1,2);
+  }
+});
+
+var sessionMinus = document.getElementById("sessionMinus");
+sessionMinus.addEventListener("click", function(){
+  resetTimer();
+  var currentValue = convertToTime(initialSessionTime);
+  currentValue -= 60;
+  initialSessionTime = convertToString(currentValue);
+  countdown.innerHTML = initialSessionTime;
+  if(initialSessionTime.charAt(0) !== "0"){
+    sessionLength.textContent = initialSessionTime.substring(0,2);
+  } else {
+    sessionLength.textContent = initialSessionTime.substring(1,2);
+  }
+});
+
+//Add time, subtract time functions
+var breakPlus = document.getElementById("breakPlus");
+breakPlus.addEventListener("click", function(){
+  resetTimer();
+  var currentValue = convertToTime(initialBreakTime);
+  currentValue += 60;
+  initialBreakTime = convertToString(currentValue);
+
+  if(timerType === "break"){
+    countdown.innerHTML = initialBreakTime;
+  }
+
+  if(initialBreakTime.charAt(0) !== "0"){
+    breakLength.textContent = initialBreakTime.substring(0,2);
+  } else {
+    breakLength.textContent = initialBreakTime.substring(1,2);
+  }
+});
+
+var breakMinus = document.getElementById("breakMinus");
+breakMinus.addEventListener("click", function(){
+  resetTimer();
+  var currentValue = convertToTime(initialBreakTime);
+  currentValue -= 60;
+  initialBreakTime = convertToString(currentValue);
+
+  if(timerType === "break"){
+    countdown.innerHTML = initialBreakTime;
+  }
+
+  if(initialBreakTime.charAt(0) !== "0"){
+    breakLength.textContent = initialBreakTime.substring(0,2);
+  } else {
+    breakLength.textContent = initialBreakTime.substring(1,2);
+  }
+});
 
 
 
@@ -98,4 +180,13 @@ function convertToTime(str){
   var sec = parseInt(seconds);
   var duration = (min*60) + sec;
   return duration;
+}
+//input number of seconds and convert that to a string formatted mm:ss
+function convertToString(seconds){
+  var min, sec;
+  min = Math.floor(seconds / 60);
+  sec = seconds % 60;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
+  return min + ":" + sec;
 }
