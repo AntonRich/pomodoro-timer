de0e53699f95dd38fa1f9c3e146dd123ac545913
Vince Campanale <vcampanale0294@gmail.com>
2016-12-04 19:33:52 -0500

    found the bug - start/pause working now

--- js/script.js
+++ js/script.js
@@ -1,47 +1,52 @@
+var startPauseText = document.getElementById("startPauseText");
+var countdown = document.getElementById("countdown");
+var duration;
+
 //initial display value of 25 minutes, type = work
 window.onload = function(){
-  var countdown = document.getElementById("countdown");
-  countdown.innerHTML = "25:00";
-  var startPauseText = document.getElementById("startPauseText");
+  countdown.textContent = "25:00";
   startPauseText.innerHTML = "Start"
 };
 
 function startTimer(duration) {
-  var startPauseText = document.getElementById("startPauseText");
   startPauseText.innerHTML = "Pause";
 
   var time = duration;
   var minutes, seconds;
+  minutes = parseInt(time / 60, 10)
+  seconds = parseInt(time % 60, 10);
 
-  setInterval(function() {
-    minutes = Math.floor(time / 60);
-    seconds = time % 60;
-    seconds = seconds < 10 ? "0" + seconds : seconds; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
-    countdown.innerHTML = minutes + ":" + seconds;
-    time--;
+  minutes = Math.floor(time / 60);
+  seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
+
+  countdown.innerHTML = minutes + ":" + seconds;
+
+  duration = duration - 1;
+
+  id = setTimeout(function () {
+      startTimer(duration);
   }, 1000);
 }
 
 function pauseTimer() {
-  var countdown = document.getElementById("countdown");
-  var currentTime = countdown.innerHTML;
-
-  var startPauseText = document.getElementById("startPauseText");
-  startPauseText.innerHTML = "Start";
+  startPauseText.textContent = "Start";
+  clearTimeout(id);
 }
 
 function handleStartPause(){
-  var startPauseText = document.getElementById("startPauseText");
-
+  var duration = convertToTime(countdown.innerHTML);
   if(startPauseText.textContent === "Start"){
-    startPauseText.textContent = "Pause";
-    var countdown = document.getElementById("countdown");
-    var duration = convertToTime(countdown.innerHTML);
+    console.log(duration);
     startTimer(duration);
     return;
   }
+
   if(startPauseText.textContent === "Pause"){
+    duration = convertToTime(countdown.innerHTML);
+    console.log(duration);
     pauseTimer();
+    return;
   }
 }
 
@@ -54,7 +59,8 @@ startPauseButton.addEventListener("click", function(){
 //input string is of the format xx : yy, returns duration in seconds
 function convertToTime(str){
   var minutes = str.substring(0, 2);
-  var seconds = str.substring(3, str.length - 1);
+  var seconds = str.substring(3, 5);
+  console.log(str.length);
   var min = parseInt(minutes);
   var sec = parseInt(seconds);
   var duration = (min*60) + sec;
