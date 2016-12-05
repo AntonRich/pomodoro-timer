7a3876083a8ca251bba28176c96977a329dab17b
Vince Campanale <vcampanale0294@gmail.com>
2016-12-04 19:48:27 -0500

    reset button working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var startPauseText = document.getElementById("startPauseText");
 var countdown = document.getElementById("countdown");
-var duration;
+var timerType; //possible values = work, break
 
 //initial display value of 25 minutes, type = work
 window.onload = function(){
@@ -10,20 +10,12 @@ window.onload = function(){
 
 function startTimer(duration) {
   startPauseText.innerHTML = "Pause";
-
-  var time = duration;
   var minutes, seconds;
-  minutes = parseInt(time / 60, 10)
-  seconds = parseInt(time % 60, 10);
-
-  minutes = Math.floor(time / 60);
-  seconds = time % 60;
+  minutes = Math.floor(duration / 60);
+  seconds = duration % 60;
   seconds = seconds < 10 ? "0" + seconds : seconds; //if seconds < 10, add a 0 before (i.e. display 2:01 rather than 2:1);
-
   countdown.innerHTML = minutes + ":" + seconds;
-
   duration = duration - 1;
-
   id = setTimeout(function () {
       startTimer(duration);
   }, 1000);
@@ -34,17 +26,19 @@ function pauseTimer() {
   clearTimeout(id);
 }
 
+function resetTimer(){
+  pauseTimer();
+  countdown.textContent = "25:00";
+}
+
+//multiple return statements less than ideal, but unsure how to handle this otherwise 
 function handleStartPause(){
   var duration = convertToTime(countdown.innerHTML);
   if(startPauseText.textContent === "Start"){
-    console.log(duration);
     startTimer(duration);
     return;
   }
-
   if(startPauseText.textContent === "Pause"){
-    duration = convertToTime(countdown.innerHTML);
-    console.log(duration);
     pauseTimer();
     return;
   }
@@ -55,12 +49,15 @@ startPauseButton.addEventListener("click", function(){
   handleStartPause();
 });
 
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", function(){
+  resetTimer();
+});
 
 //input string is of the format xx : yy, returns duration in seconds
 function convertToTime(str){
   var minutes = str.substring(0, 2);
   var seconds = str.substring(3, 5);
-  console.log(str.length);
   var min = parseInt(minutes);
   var sec = parseInt(seconds);
   var duration = (min*60) + sec;
